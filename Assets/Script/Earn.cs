567b53b8e089f1f9a522b66879fb50b80a0a4c54
최건 <highsky2224@gmail.com>
2022-03-08 00:34:56 +0900

    게임
    나무는 나중에 적립 만드는 중

---
+++ Assets/Script/Earn.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Earn : MonoBehaviour
+{
+    public int money = 0;
+    public void TouchToEarn()
+    {
+        money++;
+    }
+}
