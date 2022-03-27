a8284e12b564d159306c64b3c3a00c21ddc93e0c
최건 <highsky2224@gmail.com>
2022-03-27 22:44:20 +0900

    게임
    돈 적립

--- Assets/Script/Performance.cs
+++ Assets/Script/Performance.cs
@@ -4,7 +4,7 @@ using Newtonsoft.Json;
 
 public class Performance
 {
-    static string DefaultMoney = "1000";
+    static string DefaultMoney = "1000";    
     /* 리스트 구성 {"제품명", "초기가격", "가격 증가 폭", "성능 증가폭", "구매 횟수"}*/
     public static List<string>[] Performances = new List<string>[]
     {
@@ -18,15 +18,13 @@ public class Performance
     };
     public static string EranMoney()
     {
-        BigInteger result = BigInteger.Parse(DefaultMoney);
-        for(int idx = 0; idx < Performances.Length; idx++)
+        BigInteger result = BigInteger.Parse(DefaultMoney), Temp = BigInteger.Zero;
+        for (int idx = 0; idx < Performances.Length; idx++)
         {
-            if(Performances[idx][4] != "0")
-            {
-                BigInteger x = BigInteger.Parse(Performances[idx][1]), y = BigInteger.Parse(Performances[idx][2]);
-                BigInteger.Add(result, BigInteger.Multiply(x, BigInteger.Pow(y, int.Parse(Performances[idx][4]))));
-            }
+            result = BigInteger.Add(result, BigInteger.Multiply(BigInteger.Parse(Performances[idx][3]), BigInteger.Parse(Performances[idx][4])));
         }
+        result = BigInteger.Multiply(result, Skill.GetPercent());
+        result = BigInteger.Divide(result, BigInteger.Parse("100"));
         return result.ToString();
     }
     public static void SetPerformance(string code)
---
+++ Assets/Script/Skill.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Numerics;
+using Newtonsoft.Json;
+
+public class Skill
+{
+    /* 리스트 구성 {"제품명", "초기가격", "가격 증가 폭", "성능 증가폭", "구매 횟수"}*/
+    public static List<string>[] Skills = new List<string>[]
+    {
+        new List<string>(){ "악력 증가", "1000000", "5", "10", "0"},
+        new List<string>(){ "시력 증가", "1000000000", "5", "20", "0"},
+        new List<string>(){ "근력 강화", "1000000000000", "10", "30", "0"},
+        new List<string>(){ "체력 강화", "1000000000000000", "10", "50", "0"}
+    };
+    public static BigInteger GetPercent()
+    {
+        BigInteger percent = BigInteger.Parse("100");
+        for (int idx = 0; idx < Skills.Length; idx++)
+        {
+            percent = BigInteger.Add(percent, BigInteger.Multiply(BigInteger.Parse(Skills[idx][3]), BigInteger.Parse(Skills[idx][4])));
+        }
+        return percent;
+    }
+    public static void SetSkill(string code)
+    {
+        Skills = JsonConvert.DeserializeObject<List<string>[]>(code);
+    }
+    public static string GetSkill()
+    {
+        return JsonConvert.SerializeObject(Skills);
+    }
+}
