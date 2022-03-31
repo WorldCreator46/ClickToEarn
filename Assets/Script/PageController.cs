2e2db1913722e3a1882b37a0b43744515ec37519
최건 <highsky2224@gmail.com>
2022-04-01 00:12:41 +0900

    게임
    상점

--- Assets/Script/PageController.cs
+++ Assets/Script/PageController.cs
@@ -1,18 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PageController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] Pages;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
