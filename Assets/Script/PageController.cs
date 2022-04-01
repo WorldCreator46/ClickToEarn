d7bcff4804145e6aad64438307aa581253d630c5
최건 <highsky2224@gmail.com>
2022-04-01 16:47:20 +0900

    게임
    뭔가 많이 함

--- Assets/Script/PageController.cs
+++ Assets/Script/PageController.cs
@@ -6,5 +6,58 @@ using UnityEngine.UI;
 public class PageController : MonoBehaviour
 {
     public GameObject[] Pages;
-
+    public GameObject NextButton;
+    public GameObject BackButton;
+    public Text PageNumber;
+    public int idx = 0;
+    private void Start()
+    {
+        idx = 0;
+        Cheking();
+    }
+    public void Cheking()
+    {
+        ButtonCheck();
+        PageNumbering();
+    }
+    public void PageNumbering()
+    {
+        PageNumber.text = Pages[idx].name;
+    }
+    public void ButtonCheck()
+    {
+        if(Pages[idx].name == "1")
+        {
+            BackButton.SetActive(false);
+            NextButton.SetActive(true);
+        }
+        else if(Pages[idx].name == Pages.Length.ToString())
+        {
+            BackButton.SetActive(true);
+            NextButton.SetActive(false);
+        }
+        else
+        {
+            BackButton.SetActive(true);
+            NextButton.SetActive(true);
+        }
+    }
+    public void Next()
+    {
+        if(idx < Pages.Length-1)
+        {
+            Pages[idx].SetActive(false);
+            Pages[++idx].SetActive(true);
+            Cheking();
+        }
+    }
+    public void Back()
+    {
+        if (idx > 0)
+        {
+            Pages[idx].SetActive(false);
+            Pages[--idx].SetActive(true);
+            Cheking();
+        }
+    }
 }
